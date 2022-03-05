f2fc0b25e2dbe3f90c7a3b2aeb6b47e694605307
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-05 13:47:45 -0300

    alt

--- AprendendoCSharp/2-CriandoVariaveis/Program.cs
+++ AprendendoCSharp/2-CriandoVariaveis/Program.cs
@@ -11,8 +11,8 @@ namespace _2_CriandoVariaveis
             int idade;
             idade = 32;
 
-            float salario;
-            salario = 14500;
+            double salario;
+            salario = 1450.00;
 
             Console.WriteLine("Sir you are " + idade + " years old and your current salary is $ " + salario);
             Console.ReadLine();
