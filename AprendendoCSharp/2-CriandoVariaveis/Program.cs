83aef9f6a199554e1fe13fc25c141df816255f3b
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-05 12:43:13 -0300

    alt

--- AprendendoCSharp/2-CriandoVariaveis/Program.cs
+++ AprendendoCSharp/2-CriandoVariaveis/Program.cs
@@ -7,6 +7,12 @@ namespace _2_CriandoVariaveis
         static void Main(string[] args)
         {
             Console.WriteLine("Executando projeto 2 - Criando Variaveis");
+
+            int idade;
+            idade = 32;
+
+            Console.WriteLine(idade);
+
             Console.WriteLine("Execução finalizada. Tecle enter para sair!");
             Console.ReadLine();
         }
