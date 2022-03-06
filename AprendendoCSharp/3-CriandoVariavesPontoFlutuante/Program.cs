6b5aa2ba3cb0ce647b6cb76b44df1cb2f8f78fda
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-06 19:08:02 -0300

    alt

--- AprendendoCSharp/3-CriandoVariavesPontoFlutuante/Program.cs
+++ AprendendoCSharp/3-CriandoVariavesPontoFlutuante/Program.cs
@@ -7,8 +7,12 @@ namespace _3_CriandoVariavesPontoFlutuante
         static void Main(string[] args)
         {
             Console.WriteLine("Executando projeto 3 criando variaveis ponto flutuante");
+
+            float salario;
+            salario = 1380.50f;
+
             Console.WriteLine("A execução terminou. Tecle enter para sair...");
-            Console.ReadLine("Tecle enter para sair");
+            Console.ReadLine();
         }
     }
 }
