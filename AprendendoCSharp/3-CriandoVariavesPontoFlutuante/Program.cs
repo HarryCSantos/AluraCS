d68652621853ee9fdac8d4c87829429a79e62f0c
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-06 19:12:02 -0300

    alt

--- AprendendoCSharp/3-CriandoVariavesPontoFlutuante/Program.cs
+++ AprendendoCSharp/3-CriandoVariavesPontoFlutuante/Program.cs
@@ -11,6 +11,8 @@ namespace _3_CriandoVariavesPontoFlutuante
             float salario;
             salario = 1380.50f;
 
+            Console.WriteLine("Seu salário é de " + salario + "R$");
+
             Console.WriteLine("A execução terminou. Tecle enter para sair...");
             Console.ReadLine();
         }
