06f1b5c0333e797f95eb71c1a3b4e918154ab026
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-07 15:10:06 -0300

    alt

--- AprendendoCSharp/ConversoeseOutrosTiposNumericos/Program.cs
+++ AprendendoCSharp/ConversoeseOutrosTiposNumericos/Program.cs
@@ -7,6 +7,13 @@ namespace ConversoeseOutrosTiposNumericos
         static void Main(string[] args)
         {
             Console.WriteLine("Executando o projeto 4");
+
+            double salario;
+            salario = 1200.50;
+
+            int salarioemInteiro;
+            salarioemInteiro = (int)salario;
+
         }
     }
 }
