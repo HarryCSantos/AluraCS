73ad146170cfcc6b9a0377145e3c11cd49212816
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-07 15:36:43 -0300

    alt

--- AprendendoCSharp/ConversoeseOutrosTiposNumericos/Program.cs
+++ AprendendoCSharp/ConversoeseOutrosTiposNumericos/Program.cs
@@ -17,11 +17,14 @@ namespace ConversoeseOutrosTiposNumericos
 
             // O long é uma variavel de 64 bits
             long idade = 130000000000;
+            Console.WriteLine(idade);
 
             // O short é um tipo de variavel de 16 bits
             short quantidadedeProdutos = 16000;
+            Console.WriteLine(quantidadedeProdutos);
 
             float altura = 1.80f;
+            Console.WriteLine(altura);
 
 
 
