d34d8010ec826613f0ed96a0f5bef7a502b98e77
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-06 21:16:19 -0300

    alt.

--- AprendendoCSharp/3-CriandoVariavesPontoFlutuante/Program.cs
+++ AprendendoCSharp/3-CriandoVariavesPontoFlutuante/Program.cs
@@ -10,7 +10,7 @@ namespace _3_CriandoVariavesPontoFlutuante
             //double é um dos três tipos que aceitam numeros não inteiros os outros dois são float e decimal
             
             double salario;
-            salario = 1380.50;
+            salario = 1250.70;
 
             Console.WriteLine("Seu salário é de R$ " + salario);
 
