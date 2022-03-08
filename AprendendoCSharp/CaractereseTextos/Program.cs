8ac3f279d5df4fc3ee618e014fe6c230fb5718c7
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-08 19:42:03 -0300

    alt

--- AprendendoCSharp/CaractereseTextos/Program.cs
+++ AprendendoCSharp/CaractereseTextos/Program.cs
@@ -23,7 +23,7 @@ namespace CaractereseTextos
 
             string curso = "Alura Cursos de tecnologia " + 2020;
             string cursosProgramacao = 
-  @"- .Net
+     @"- .Net
  - Java
  - Javascript";
             Console.WriteLine(cursosProgramacao);
