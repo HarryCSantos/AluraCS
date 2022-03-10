50215d3840fe243d921988beeaf6610091affb11
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-10 19:37:45 -0300

    alt

---
+++ AprendendoCSharp/1-TestantoCondicionais/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_TestantoCondicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testando condicionais");
+        }
+    }
+}
