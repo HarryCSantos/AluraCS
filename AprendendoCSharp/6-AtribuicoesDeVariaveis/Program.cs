2b690bff991d60eb746c480797fd74746e5dbdd0
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-09 22:18:43 -0300

    alt

--- AprendendoCSharp/6-AtribuicoesDeVariaveis/Program.cs
+++ AprendendoCSharp/6-AtribuicoesDeVariaveis/Program.cs
@@ -6,7 +6,7 @@ namespace _6_AtribuicoesDeVariaveis
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Executando Projeto 6");
         }
     }
 }
