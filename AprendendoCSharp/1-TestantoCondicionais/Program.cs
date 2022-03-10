7e564bcd6125296f592089555c51838da6f91394
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-10 20:07:06 -0300

    alt

--- AprendendoCSharp/1-TestantoCondicionais/Program.cs
+++ AprendendoCSharp/1-TestantoCondicionais/Program.cs
@@ -14,17 +14,17 @@ namespace _1_TestantoCondicionais
             if (idade >= 18)
             {
                 Console.WriteLine("Você tem mais que 18 anos");
-                Console.WriteLine("Seja bem vindo");
+                Console.WriteLine("Seja bem vindo!");
             }
             else
             {
                 if (quantidadePessoas >= 2)
                 {
-                    Console.WriteLine("Você não tem 18 anos, mas pode entrar, "+ "pois está acompanhado");
+                    Console.WriteLine("Você não tem 18 anos, mas pode entrar, "+ "pois está acompanhado.");
                 }
                 else
                 {
-                    Console.WriteLine("Infelizmente você não pode entrar pois é menor de idade e não está acompanhado");
+                    Console.WriteLine("Infelizmente você não pode entrar pois é menor de idade e não está acompanhado.");
                 }
             }
 
